6aefbc0a992a692419c877248819ce23b377095a
Krishna-Prakaash <suruthikrish11805@gmail.com>
2024-07-17 22:18:52 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <ul>
+      <li>Hello there !! I am KRISHNA PRAKAASH , a final-year student currently pursuing Artificial Intelligence & Data Science in Saveetha Engineering College, CHENNAI.</li>
+      <li>I Did my schooling in BVB Matriculation Hr.Sec.School ,Palani.</li>
+      <li>I have scored 91% in State Board Examination and currently with a CGPA of 8.0 as of semester-6.</li>
+      <li>I am intrested in C, Python, C#, MySql, Database, HTML, CSS, React and Javascript. </li>
+      <li>I Did  internship in Edify Techno Solutions on Machine learning. </li>
+      <li>Languages known are English, Tamil and German.</li>
+      <li>Softwares used : Git and Github , blender ,Canva, Fusion 360, VS Code, Unity </li>
+    </ul>
+  </section>
+);
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <ul>
+      <li><a href="mailto:your-email@example.com">suruthikrishs11805@gmail.com</a></li>
+      <li><a href="mailto:your-email@example.com">https://www.linkedin.com/in/krishna-prakaash-b6b811226/</a></li>
+      <li><a href="mailto:your-email@example.com">https://github.com/Krishna-Prakaash</a></li>
+      <li>Contact: 6382159698 / 6382159798</li>
+    </ul>
+  </section>
+);
+
+export default Contact;
